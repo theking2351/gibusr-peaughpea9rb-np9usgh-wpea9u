2d6452b6c17fb18033e638e55dfce31a844d78e3
theking2351 <76833292+theking2351@users.noreply.github.com>
2021-08-11 19:15:55 +0530

    Add files via upload

---
+++ js/Block.js
@@ -0,0 +1,36 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+            restitution :0.4,
+            friction :0.0,
+            isStatic : false
+        }
+        
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+         this.image= loadImage("block.png")
+        World.add(world, this.body);
+      }
+
+      display(){
+
+        var pos= this.body.position;
+        imageMode(CENTER);
+
+        if(this.body.speed <3){
+          image(this.image,pos.x,pos.y,this.width, this.height);
+        }
+        else{
+
+          World .remove(world,this.body);
+          push ()
+          this.Visiblity=this.Visiblity-5;
+          tint (255,this.Visiblity)
+          image(this.image,this.body.position.x,this.body.position.y,50,50)
+          pop ();
+          
+
+        }
+}
+}
